cf5e5127455f5a0f6fc4b7232f95754f44b967e3
Olga Berenice Hernández Cruz <obhernandezc@gbm.com.mx>
2024-03-27 10:51:45 -0600

    Add html to pdf conversor example

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using IronPdf;
+using IronPdf.Rendering;
+
+IronPdf.License.LicenseKey = "LICENSE-KEY";
+
+string ResourcePath = @"HTML-files/source";
+string DestinationDirectory = @"destination/directory";
+
+Random Rand = new Random(Math.Abs((int)DateTime.Now.Ticks));
+
+var Renderer = new ChromePdfRenderer(); // Instantiates Chrome Renderer
+
+var resultPath = @$"{DestinationDirectory}\ResultPDF-{Rand.Next()}.pdf";
+
+Renderer.RenderingOptions.PaperSize = IronPdf.Rendering.PdfPaperSize.A4;
+Renderer.RenderingOptions.PaperOrientation = PdfPaperOrientation.Portrait;
+
+Renderer.RenderingOptions.CssMediaType = PdfCssMediaType.Print;
+
+var pdf = Renderer.RenderHtmlFileAsPdf($@"{ResourcePath}\*.html");
+
+pdf.SaveAs(resultPath); // Saves our PdfDocument object as a PDF
